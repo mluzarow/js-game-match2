3981bb26ef004729ebb701c709be7d4b0c12d88d
Mackenzie <ma.luzarow@gmail.com>
2018-04-22 16:22:50 -0500

    Added js files.

--- js/MatchTwo.js
+++ js/MatchTwo.js
@@ -1,1 +1,73 @@
-
+class MatchTwo {
+	constructor (nTiles, time, imageList) {
+		this.numTiles = nTiles;
+		this.time = time;
+		
+		this.images = this.setImages (this.numTiles, imageList);
+		
+		this.drawTiles (this.numTiles, this.images);
+	}
+	
+	setImages (nTiles, imageList) {
+		if ((nTiles / 2) > imageList.length) {
+			// Duplication images until we have enough
+			var delta = (nTiles / 2) - imageList.length;
+			
+			for (var i = 0; i < delta; i++) {
+				imageList.push (imageList[i]);
+			}
+		}
+		
+		// We have exactly the amount we need; now duplicate the list.
+		imageList.concat (imageList);
+		
+		// Shuffle the images
+		var newImages = [];
+		var len = imageList.length;
+		
+		for (var i = 0; i < len; i++) {
+			let index = Math.floor(Math.random() * (imageList.length));
+			
+			newImages.push (imageList[index]);
+			
+			imageList.splice (index, 1);
+		}
+		
+		return (newImages);
+	}
+	
+	drawTiles (nTiles, images) {
+		var board = document.getElementById ("board-wrap");
+		
+		for (var i = 0; i < nTiles; i++) {
+			board.appendChild (this.makeTile (images[i]));
+		}
+	}
+	
+	makeTile (img) {
+		var tile = document.createElement ("div");
+		tile.classList.add ("cell-wrap");
+		
+		var cell = document.createElement ("div");
+		cell.classList.add ("cell");
+		
+		var imgTag = document.createElement ("img");
+		imgTag.src = img;
+		
+		cell.appendChild (imgTag);
+		tile.appendChild (cell);
+		
+		tile.addEventListener ("click", function (e) {
+			var self = e.target;
+			
+			console.log (self);
+			if (self.classList.value.indexOf ("flipped") !== -1) {
+				self.classList.remove ("flipped");
+			} else {
+				self.classList.add ("flipped");
+			}
+		});
+		
+		return (tile);
+	}
+}
---
+++ js/Setup.js
@@ -0,0 +1,61 @@
+class Setup {
+	constructor (imageList) {
+		this.imageList = imageList;
+		
+		this.$setupWrap = document.getElementById ("setup-wrap");
+		
+		this.$inputImages = document.getElementsByClassName("images")[0];
+		this.$inputTiles = document.getElementsByClassName("tiles")[0];
+		this.$inputTime = document.getElementsByClassName("time")[0];
+		
+		this.$wrapImages = this.$inputImages.parentNode;
+		this.$wrapTiles = this.$inputTiles.parentNode;
+		this.$wrapTime = this.$inputTime.parentNode;
+		
+		this.$startBtn = document.getElementById("setup-start_btn");
+		
+		this.$inputImages.value = "8";
+		this.$inputTiles.value = "16";
+		this.$inputTime.value = "60";
+		
+		this.inputValidation = new RegExp("^[0-9]+$");
+		
+		this.$startBtn.addEventListener("click", this.startButtonClick.bind (this));
+	}
+	
+	startButtonClick () {
+		var flagImages = this.inputValidation.test(this.$inputImages.value);
+		var flagTiles = this.inputValidation.test(this.$inputTiles.value);
+		var flagTime = this.inputValidation.test(this.$inputTime.value);
+		
+		this.$wrapImages.classList.remove ("error");
+		this.$wrapTiles.classList.remove ("error");
+		this.$wrapTime.classList.remove ("error");
+		
+		if ((flagImages && flagTiles && flagTime) === false) {
+			if (flagImages === false) {
+				this.$wrapImages.classList.add ("error");
+			}
+			
+			if (flagTiles === false) {
+				this.$wrapTiles.classList.add ("error");
+			}
+			
+			if (flagTime === false) {
+				this.$wrapTime.classList.add ("error");
+			}
+		} else {
+			this.loadGame ();
+		}
+	}
+	
+	loadGame () {
+		this.$setupWrap.style.display = "none";
+		
+		new MatchTwo (
+			parseInt (this.$inputImages.value),
+			parseInt (this.$inputTime.value),
+			this.imageList
+		);
+	}
+}
